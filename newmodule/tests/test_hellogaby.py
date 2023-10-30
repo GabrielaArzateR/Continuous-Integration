637e106c8517e682ecd7d8c13537a5714512ca6d
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-10-30 16:31:48 +0100

    modifications from helloword to hellogabyy newone

---
+++ newmodule/tests/test_hellogaby.py
@@ -0,0 +1,8 @@
+"""Dummy module description"""
+from newmodule.hellogaby import hellogaby
+
+
+def test_hellogaby() -> None:
+    """Tests the hello gaby function"""
+    hellogaby()
+    assert True
